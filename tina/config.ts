85e603748146d124e4b7689b2c1dac3e6ebc8202
linhx <linhx1999@163.com>
2024-12-16 10:18:48 +0800

    Update TinaCMS configuration and media paths in config.ts;

--- tina/config.ts
+++ tina/config.ts
@@ -15,8 +15,11 @@ export default defineConfig({
 	},
 	media: {
 		tina: {
-			mediaRoot: '/src/assets/images',
-			publicFolder: ''
+			// mediaRoot: '/src/assets/images',、
+			// publicFolder: ''
+
+			mediaRoot: 'images',
+			publicFolder: '/src/assets'
 		}
 	},
 	schema: {
