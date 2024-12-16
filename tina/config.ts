28cacdbc66df04d67556a9f81db1d6b56e5ee30c
linhx <linhx1999@163.com>
2024-12-16 10:30:47 +0800

    Refactor TinaCMS configuration: enable media paths in config.ts and update tina-lock.json to reflect schema changes.

--- tina/config.ts
+++ tina/config.ts
@@ -15,11 +15,11 @@ export default defineConfig({
 	},
 	media: {
 		tina: {
-			// mediaRoot: '/src/assets/images',、
-			// publicFolder: ''
+			mediaRoot: '/src/assets/images',
+			publicFolder: ''
 
-			mediaRoot: 'images',
-			publicFolder: '/src/assets'
+			// mediaRoot: 'images',
+			// publicFolder: '/src/assets'
 		}
 	},
 	schema: {
